908c6f4063bc776a6d24ab997f22e9fd0f21a909
Xar0P <pietroricardocres@hotmail.com>
2022-06-03 15:56:35 -0300

    navbar mobile finished

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Mobile from './NavBar.mobile';
 import Desktop from './NavBar.desktop';
 
-const NavBar: React.FC = () => (
-  <>
-    <Mobile />
-    {/* <Desktop /> */}
-  </>
-);
+const NavBar: React.FC = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+  }, []);
+
+  return windowWidth < 768 ? <Mobile /> : <Desktop />;
+};
 
 export default NavBar;
