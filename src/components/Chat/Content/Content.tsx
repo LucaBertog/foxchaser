04b0263677286ac098f0b13f14280ae8dd6be973
Xar0P <pietroricardocres@hotmail.com>
2022-08-06 17:47:22 -0300

    change name and profilePicture in chat

--- src/components/Chat/Content/Content.tsx
+++ src/components/Chat/Content/Content.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import { skipToken } from '@reduxjs/toolkit/dist/query';
 import {
   Container,
   Header,
@@ -13,22 +14,38 @@ import {
 } from './Content.styles';
 import NoPP from '../../../assets/imgs/NoPP.png';
 import Messages from './Messages/Messages';
+import { selectUserSelected } from '../../../store/Chat/reducer';
+import { useGetUserByIdQuery } from '../../../services/api/user.api';
+import LogoLoader from '../../LogoLoader/LogoLoader';
 
-const Content: React.FC = () => (
-  <Container>
-    <Header>
-      <Wrapper>
-        <Image src={NoPP} />
-        <Name>Churumelas</Name>
-      </Wrapper>
-      <MoreOptions />
-    </Header>
-    <Messages />
-    <NewMessageWrapper>
-      <AttachIcon />
-      <NewMessage type='text' placeholder='Escreva aqui...' />
-    </NewMessageWrapper>
-  </Container>
-);
+const Content: React.FC = () => {
+  const userSelected = useSelector(selectUserSelected);
+  const { data: user, isLoading } = useGetUserByIdQuery(
+    userSelected || skipToken
+  );
+
+  return (
+    <Container>
+      {isLoading ? (
+        <LogoLoader />
+      ) : (
+        <>
+          <Header>
+            <Wrapper>
+              <Image src={user?.user.profilePicture.split(' ')[0] || NoPP} />
+              <Name>{user?.user.name}</Name>
+            </Wrapper>
+            <MoreOptions />
+          </Header>
+          <Messages />
+          <NewMessageWrapper>
+            <AttachIcon />
+            <NewMessage type='text' placeholder='Escreva aqui...' />
+          </NewMessageWrapper>
+        </>
+      )}
+    </Container>
+  );
+};
 
 export default Content;
